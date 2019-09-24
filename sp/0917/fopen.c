011d9523a986b7fb6ee2d38a3ebd273f110b8d6e
sji8562 <whaqlzkem@naver.com>
2019-09-24 09:14:29 +0900

    Third

---
+++ sp/0917/fopen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+int main(int argc, char *argv[])
+{
+	int fd;
+	if((fd = open(argv[1], O_RDWR)) == -1)
+		printf("file open error\n");
+	else
+		printf("file %s open success : %d\n", argv[1], fd);
+	close(fd);
+	exit(0);
+}
+
