90f36d8ecc08259f33479d844ce5f7147c8eefe3
sji8562 <whaqlzkem@naver.com>
2019-11-26 10:36:12 +0900

    11월 26일 수업

---
+++ sp/1126/inthandler.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+void intHandler();
+
+int main(){
+	signal(SIGINT,intHandler);
+	while(1)
+		pause();
+	printf("실행되지 않음\n");
+}
+void intHandler(int signo){
+	printf("인터럽트 시그널 처리\n");
+	printf("시그널 번호: %d\n",signo);
+	exit(0);
+}
